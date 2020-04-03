e7d7211a656d097bbb84e4ea9251fec69ca94452
Popov_Ivan <ivanwork137@yandex.ru>
2020-04-03 21:29:27 +0300

    Initial commit

---
+++ src/main/java/ru/sporting/goods/rental/SportingGoodsRentalApplication.java
@@ -0,0 +1,13 @@
+package ru.sporting.goods.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportingGoodsRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportingGoodsRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sporting/goods/rental/SportingGoodsRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sporting.goods.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportingGoodsRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
